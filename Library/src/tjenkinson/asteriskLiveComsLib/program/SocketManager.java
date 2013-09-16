1eaa76fe0c463e56f94bb8faba09535e0fca67f0
Tom Jenkinson <tjcode@outlook.com>
2013-09-17 00:03:19 +0100

    Now detects loss of connection better.

--- Library/src/tjenkinson/asteriskLiveComsLib/program/SocketManager.java
+++ Library/src/tjenkinson/asteriskLiveComsLib/program/SocketManager.java
@@ -9,8 +9,15 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import tjenkinson.asteriskLiveComsLib.program.events.AsteriskLiveComsEvent;
 import tjenkinson.asteriskLiveComsLib.program.events.AsteriskLiveComsEventListener;
@@ -46,13 +53,48 @@ public class SocketManager {
     
     private String ip;
     private int port;
-
+    
+    private Timer connectionCheckTimer;
+    
+    private ExecutorService socketRequestExecutor = Executors.newCachedThreadPool();
+	private Callable<String> socketRequestTask = new Callable<String>() {
+		public String call() {
+			String a = null;
+			try {
+				a = incomingResponseIn.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return a;
+		}
+	};
+    
     private Object socketLock = new Object();
 
     public SocketManager(String ip, int port) {
         this.ip = ip;
         this.port = port;
         eventsDispatcherExecutor = Executors.newSingleThreadExecutor();
+        connectionCheckTimer = new Timer();
+        // send a ping request every 10 seconds to make sure the connection is still alive
+        connectionCheckTimer.schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				if (isConnected()) {
+					// only ping if believed to be connected
+					JSONObject request = new JSONObject();
+					request.put("action", "ping");
+					try {
+						sendRequest(request);
+					} catch (JSONException e) {
+						e.printStackTrace();
+					} catch (NotConnectedException e) {
+					}
+				}
+			}
+        	
+        }, 10000, 10000);
     }
 
     public void connect() throws UnableToConnectException, AlreadyConnectedException {
@@ -147,18 +189,38 @@ public class SocketManager {
     	}
 	}
     
-    public JSONObject sendRequest(JSONObject request) throws JSONException, NotConnectedException {
+    public JSONObject sendRequest(final JSONObject request) throws JSONException, NotConnectedException {
     	synchronized(socketLock) {
-	    	out.println(request.toString());
-	    	try {
-				return new JSONObject(incomingResponseIn.readLine());
-			} catch (IOException e) {
-				// lets close the connection
+    		out.println(request.toString());
+    		String response = null;
+    		Future<String> future = socketRequestExecutor.submit(socketRequestTask);
+    		try {
+    			// give the server 15 seconds to respond.
+    			response = future.get(15, TimeUnit.SECONDS); 
+    		} catch (TimeoutException ex) {
+    			// taking longer than 15 seconds
+    			System.out.println("Heard no response to request in 15 seconds. Presuming the server has lost connection.");
+    		} catch (InterruptedException e) {
+    			e.printStackTrace();
+    		} catch (ExecutionException e) {
+    			e.printStackTrace();
+    		}
+    		finally {
+    			// attempt to stop the task if it's still waiting
+    			future.cancel(true);
+    		}
+    		
+    		if (response == null) {
+    			// no response. it timed out
+    			// lets close the connection
 				try {
 					disconnect(true);
 				} catch (NotConnectedException e1) {}
 				throw(new NotConnectedException());
-			}
+    		}
+    		// it appears that everything happened successfully
+    		// return the response
+	    	return new JSONObject(response);
     	}
     }
 
